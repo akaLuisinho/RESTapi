c680f6bc7fb74067084faec9b22c3465204c31bd
Luis Felipe <luis.felipeszsv@gmail.com>
2021-06-15 21:30:21 -0300

    creating comment function

--- src/jwt.js
+++ src/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
 
-const secret = 'ratatouille'
+const secret = '7011B12039B74A2748DA225C9EE568E8FDA4FF9771DCE6B36B1950557E3F8D3C'
 
 sign = (payload) => jwt.sign(payload, secret, { expiresIn: 86400 })
 verify = (token) => jwt.verify(token, secret)
